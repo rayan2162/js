f6714c5d58f670e541384a49b01bd62900ba58a6
rayan2162 <134594995+rayan2162@users.noreply.github.com>
2026-02-02 20:08:59 +0600

    init

---
+++ for-in-loop.js
@@ -0,0 +1,11 @@
+let student = {
+    name:"rayan",
+    age:"25",
+    dept:"cse",
+    cgpa:"3.4",
+    isPassed:true
+}
+
+for(let val in student){
+    console.log(val)
+}
--- for-of-loop.js
+++ for-of-loop.js
@@ -2,16 +2,3 @@ let str = "hello";
 for(let i of str){
     console.log(i);
 }
-
-
-let student = {
-    name:"rayan",
-    age:"25",
-    dept:"cse",
-    cgpa:"3.4",
-    isPassed:true
-}
-
-for(let val in student){
-    console.log(val)
-}
