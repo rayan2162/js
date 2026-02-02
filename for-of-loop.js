5e3689c90da57dad5d61c5a3492ee191987300b4
Rayanpucit <rayanul@it.puc.ac.bd>
2026-02-02 17:42:33 +0600

    init

---
+++ for-of-loop.js
@@ -0,0 +1,17 @@
+let str = "hello";
+for(let i of str){
+    console.log(i);
+}
+
+
+let student = {
+    name:"rayan",
+    age:"25",
+    dept:"cse",
+    cgpa:"3.4",
+    isPassed:true
+}
+
+for(let val in student){
+    console.log(val)
+}
